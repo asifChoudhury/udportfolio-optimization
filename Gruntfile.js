9f8bdaa9a9848706893616f6d913c83fe864a8f2
MD Asif Choudhury <bdl365@yahoo.com>
2016-05-16 16:04:24 -0400

    minified html

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,54 +7,54 @@ module.exports = function(grunt) {
         /* Clear out the images directory if it exists */
         clean: {
           dev: {
-            src: ['img/compressed', 'views/images/compressed']
+            src: ['index.html', 'project-2048.html', 'project-mobile.html', 'project-webperf.html']
           },
         },
 
         /* Generate the images directory if it is missing */
-        mkdir: {
-          dev: {
-            options: {
-              create: ['img/compressed', 'views/images/compressed']
-            },
-          },
-        },
+        // mkdir: {
+        //   dev: {
+        //     options: {
+        //       create: ['img/compressed', 'views/images/compressed']
+        //     },
+        //   },
+        // },
 
-        cssmin: {
-          target: {
-            files: [{
-              expand: true,
-              cwd: 'css/',    //css file directory
-              src: ['*.css'], //select all css files
-              dest: 'css/',   //save it in specified directory
-              ext: '.min.css'
-            }]
-          }
-        },
+        // cssmin: {
+        //   target: {
+        //     files: [{
+        //       expand: true,
+        //       cwd: 'css/',    //css file directory
+        //       src: ['*.css'], //select all css files
+        //       dest: 'css/',   //save it in specified directory
+        //       ext: '.min.css'
+        //     }]
+        //   }
+        // },
 
-        uglify: {
-          build: {
-            src: 'js/perfmatters.js',      //js file directory
-            dest: 'js/perfmatters.min.js'  //save it in specified directory
-          }
-        },
+        // uglify: {
+        //   build: {
+        //     src: 'js/perfmatters.js',      //js file directory
+        //     dest: 'js/perfmatters.min.js'  //save it in specified directory
+        //   }
+        // },
 
-        imagemin: {
-          dynamic: {
-            files: [{
-              expand: true,
-              cwd: 'img/',
-              src: ['**/*.{png,jpg,gif}'],
-              dest: 'img/compressed/'
-            },
-            {
-              expand: true,
-              cwd: 'views/images/',
-              src: ['**/*.{png,jpg,gif}'],
-              dest: 'views/images/compressed/'
-            }]
-          }
-        },
+        // imagemin: {
+        //   dynamic: {
+        //     files: [{
+        //       expand: true,
+        //       cwd: 'img/',
+        //       src: ['**/*.{png,jpg,gif}'],
+        //       dest: 'img/compressed/'
+        //     },
+        //     {
+        //       expand: true,
+        //       cwd: 'views/images/',
+        //       src: ['**/*.{png,jpg,gif}'],
+        //       dest: 'views/images/compressed/'
+        //     }]
+        //   }
+        // },
         htmlmin: {                                     // Task
           dist: {                                      // Target
             options: {                                 // Target options
@@ -75,12 +75,12 @@ module.exports = function(grunt) {
 
     // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-mkdir');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    // grunt.loadNpmTasks('grunt-contrib-copy');
+    // grunt.loadNpmTasks('grunt-mkdir');
+    // grunt.loadNpmTasks('grunt-contrib-cssmin');
+    // grunt.loadNpmTasks('grunt-contrib-uglify');
+    // grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['clean', 'mkdir', 'cssmin', 'uglify', 'imagemin', 'htmlmin']);
+    grunt.registerTask('default', ['clean', /*'mkdir', 'cssmin', 'uglify', 'imagemin', */'htmlmin']);
 };
